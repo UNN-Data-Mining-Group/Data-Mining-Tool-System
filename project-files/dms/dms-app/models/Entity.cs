bf2ccaafd2889a0ea6ad5d14d8776893be118bbe
juramoshkov <juramoshkov@gmail.com>
2016-12-12 12:00:20 +0300

    Added database managers, entity class, update class diagram

---
+++ project-files/dms/dms-app/models/Entity.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using dms.services;
+
+namespace dms.models
+{
+    class Entity
+    {
+        private int id = -1;
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        private string nameTable;
+        public string NameTable
+        {
+            get
+            {
+                return nameTable;
+            }
+            set
+            {
+                nameTable = value;
+            }
+        }
+
+        public static string PrimaryKey()
+        {
+            return "ID";
+        }
+
+        public Dictionary<string, string> mappingTable()
+        {
+            /*
+             * Возвращает таблицу соответствий свойств класса, и аттрибутов сущности в БД
+             * key соответствует названию свойства класса, value - аттрибут сущности
+             */
+            Dictionary<string, string> mappingTable = new Dictionary<string, string>();
+            mappingTable.Add("id", "ID");
+            return mappingTable;
+        }
+
+        public Dictionary<string, string> serializationParameters()
+        {
+            return null;
+        }
+
+        public void save()
+        {
+            DatabaseManager.sharedManager.saveEntity(this);
+        }
+
+        public void delete()
+        {
+            DatabaseManager.sharedManager.deleteEntity(this);
+        }
+
+        public static Entity getById(int id)
+        {
+            return null;//DatabaseManager.sharedManager.entityById(this.NameTable, id);
+        }
+
+        public static List<Entity> all()
+        {
+            return null;
+        }
+
+        public static List<Entity> where(Query query)
+        {
+            return null;
+        }
+    }
+    
+}
---
+++ project-files/dms/dms-app/services/DatabaseManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+using dms.models;
+
+namespace dms.services
+{
+    class DatabaseManager
+    {
+        public static DatabaseManager sharedManager = new DatabaseManager();
+
+        public void saveEntity(Entity entity)
+        {
+            Query query = new Query(entity.NameTable);
+            Dictionary<string, string> changedValues = new Dictionary<string, string>();
+            PropertyInfo []properties = entity.GetType().GetProperties();
+            Dictionary<string, string>mappingTable = entity.mappingTable();
+            foreach (PropertyInfo property in properties)
+            {
+                changedValues.Add(mappingTable[property.Name], property.GetValue(entity).ToString());
+            }
+            if (entity.ID == -1)
+            {
+                query.addTypeQuery(TypeQuery.insert);
+            }
+            else
+            {
+                query.addTypeQuery(TypeQuery.update)
+                    .addCondition(Entity.PrimaryKey(), "=", entity.ID.ToString());
+            }
+            query.setChangedValues(changedValues);
+            int insertId = executeUpdateInsertQuery(query);
+            if (entity.ID == -1)
+            {
+                entity.ID = insertId;
+            }
+        }
+
+        public void deleteEntity(Entity entity)
+        {
+            if (entity.ID == -1)
+            {
+                return;
+            }
+            Query query = new Query(entity.NameTable);
+            query.addTypeQuery(TypeQuery.delete)
+                    .addCondition(Entity.PrimaryKey(), "=", entity.ID.ToString());
+            executeDeleteQuery(query);
+        }
+
+        public Entity entityById(string nameTable, int id)
+        {
+            return null;
+        }
+
+        public List<Entity> entitiesByQuery(Query query)
+        {
+            return null;
+        }
+
+        public List<Entity> allEntities(string nameTable)
+        {
+            return null;
+        }
+
+        // private sqlconnector
+        // private sqlconnection
+
+        private int executeUpdateInsertQuery(Query query)
+        {
+            int insertId = 0;
+            return insertId;
+        }
+
+        private void executeDeleteQuery(Query query)
+        {
+            
+        }
+    }
+}
